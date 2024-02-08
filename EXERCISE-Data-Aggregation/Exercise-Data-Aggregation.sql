23e93767a121b8b72618ee64defd1e773d079dcc
Dxn1el <126982891+Dxn1el@users.noreply.github.com>
2024-02-09 01:51:43 +0200

    Update Exercise-Data-Aggregation.sql

--- EXERCISE-Data-Aggregation/Exercise-Data-Aggregation.sql
+++ EXERCISE-Data-Aggregation/Exercise-Data-Aggregation.sql
@@ -1,8 +1,8 @@
-#1. records count
+# records count
 
 SELECT COUNT(*) AS 'count' FROM `wizzard_deposits`;
 
-#2. longest magic want
+# longest magic want
 SELECT MAX(`magic_wand_size`) AS 'Longest Wand' FROM `wizzard_deposits`;
 
 # NOT A SUBMISSION : 2 longest magic want 2.1
@@ -10,20 +10,20 @@ SELECT DISTINCT `magic_wand_size`FROM `wizzard_deposits`
  ORDER BY `magic_wand_size` DESC
  LIMIT 1 OFFSET 1;
  
- #3. longest magic wand per deposit group
+ # longest magic wand per deposit group
  SELECT `deposit_group`, MAX(`magic_wand_size`) AS'longest_magic_wand' FROM `wizzard_deposits`
  GROUP BY `deposit_group`
  ORDER BY `longest_magic_wand`, `deposit_group`
 ;
 
-#4. smallest magic wand per deposit group
+# smallest magic wand per deposit group
 
 SELECT `deposit_group`, MIN(`magic_wand_size`) AS `smallest_magic_wand` FROM `wizzard_deposits`
 GROUP BY `deposit_group`
 ORDER BY `smallest_magic_wand`, `deposit_group`
 ;
 
-#5. smallest deposit group per magic wand size
+# smallest deposit group per magic wand size
 
 SELECT `deposit_group` FROM `wizzard_deposits`
 GROUP BY `deposit_group`
@@ -31,31 +31,31 @@ ORDER BY AVG(`magic_wand_size`) LIMIT 1;
 # get all sum
 SELECT SUM(`deposit_amount`) FROM `wizzard_deposits`;
 
-#6.  get sum per deposit group
+#  get sum per deposit group
 SELECT `deposit_group`, SUM(`deposit_amount`) AS'Suma'FROM `wizzard_deposits`
 GROUP BY`deposit_group`
 ORDER BY `Suma`, `deposit_group`;
 
-#7. deposit sum for ollivander family
+#deposit sum for ollivander family
 
 SELECT `deposit_group`, SUM(`deposit_amount`) AS'Suma'FROM `wizzard_deposits`
 WHERE `magic_wand_creator`='Ollivander Family'
 GROUP BY `deposit_group`
 ORDER BY `deposit_group`;
 
-#8. deposit filter
+# deposit filter
 SELECT `deposit_group`, SUM(`deposit_amount`) AS'Suma'FROM `wizzard_deposits`
 WHERE `magic_wand_creator`='Ollivander Family'
 GROUP BY `deposit_group`
 HAVING `Suma`<150000
 ORDER BY `Suma` DESC;
 
-#9. deposit change
+# deposit change
 SELECT `deposit_group`,`magic_wand_creator`,MIN(`deposit_charge`) FROM `wizzard_deposits`
 GROUP BY`deposit_group`,`magic_wand_creator`
 ORDER BY `magic_wand_creator`,`deposit_group` ASC;
 
-#10.AGE GROUPS
+#AGE GROUPS
 
 SELECT  (
 CASE
@@ -72,16 +72,77 @@ GROUP BY `age_group`
 ORDER BY `age_group`
 ;
 
-#11. unique letters
+# unique letters
 
 SELECT substring(`first_name`,1,1) AS 'letter' FROM `wizzard_deposits`
 WHERE `deposit_group` = 'Troll Chest'
 GROUP BY `letter`
 Order by `letter`;
 
-#12. Employees Minimum Salaries
-#13. Employees Average Salarie
-#14. Employees Maximum Salaries
-#15. Employees Count Salaries
-#17. Salary Challenge**
-#18. Departments Total Salaries
+
+# Average Interest 
+SELECT `deposit_group`,`is_deposit_expired`,AVG(deposit_interest) FROM `wizzard_deposits`
+WHERE `deposit_start_date` >'1985-01-01'
+GROUP BY `deposit_group`, `is_deposit_expired`
+ORDER BY `deposit_group` DESC, `is_deposit_expired` ASC;
+
+# Employees Minimum Salaries
+
+SELECT `department_id`, MIN(`salary`) FROM `employees`
+WHERE `hire_date`> '2000-01-01' AND( `department_id`=2 OR `department_id`=7 OR `department_id`=5)
+GROUP BY `department_id`
+ORDER BY `department_id` ASC;
+
+
+# Employees Average Salarie
+
+CREATE TABLE `high_paid_employees` AS
+SELECT * FROM `employees` 
+WHERE salary>30000;
+
+DELETE FROM `high_paid_employees`
+WHERE `manager_id`=42;
+
+UPDATE `high_paid_employees`
+SET `salary` =  `salary` + 5000
+WHERE `department_id`=1;
+
+SELECT `department_id`,AVG(`salary`) FROM `high_paid_employees`
+GROUP BY `department_id`
+ORDER BY `department_id` ASC;
+
+# Employees Maximum Salaries
+SELECT `department_id`, MAX(`salary`) AS `max_salary` FROM `employees`
+GROUP BY `department_id`
+HAVING `max_salary` NOT BETWEEN 30000 AND 70000
+ORDER BY `department_id`;
+
+# Employees Count Salaries
+SELECT COUNT(`salary`) FROM `employees`
+WHERE `manager_id` IS NULL;
+
+# 3rd Highest Salary* 
+SELECT DISTINCT `department_id`, (
+SELECT DISTINCT `salary`  FROM `employees` e
+WHERE e.`department_id`= `employees`.`department_id`
+ORDER BY `salary` DESC
+LIMIT 1 OFFSET 2
+) AS 'third_highest_salary' FROM `employees`
+GROUP BY `department_id`
+HAVING `third_highest_salary` IS NOT NULL
+ORDER BY `department_id`;
+
+# Salary Challenge**
+
+SELECT `first_name`, `last_name`, `department_id` FROM `employees` AS e1
+WHERE `salary`>(
+SELECT AVG(`salary`) FROM `employees` e2
+WHERE e1.`department_id`= e2.`department_id`
+)
+ORDER BY `department_id`, `employee_id`
+LIMIT 10;
+
+# Departments Total Salaries
+SELECT `department_id`, SUM(`salary`) FROM `employees`
+GROUP BY `department_id`
+ORDER BY `department_id`;
