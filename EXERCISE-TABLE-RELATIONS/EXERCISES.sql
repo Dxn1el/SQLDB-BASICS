27aa88335d04d5f6837132f024059d6fba2ce61d
Dxn1el <126982891+Dxn1el@users.noreply.github.com>
2024-02-09 17:06:49 +0200

    Update EXERCISES.sql

--- EXERCISE-TABLE-RELATIONS/EXERCISES.sql
+++ EXERCISE-TABLE-RELATIONS/EXERCISES.sql
@@ -28,7 +28,7 @@ VALUES
 ('Tom',56100,103),
 ('Yana',60200,101);
 
-#One-To-Many Relationship
+# not part of the exercise
 
 ALTER TABLE `tablerelations`.`people` 
 DROP FOREIGN KEY `fk_passport`;
@@ -36,3 +36,35 @@ ALTER TABLE `tablerelations`.`people`
 DROP INDEX `passport_id` ;
 ;
 
+#One-To-Many Relationship
+CREATE DATABASE `cars`;
+
+CREATE TABLE `manufacturers`(
+`manufacturer_id` INT AUTO_INCREMENT PRIMARY KEY,
+`name` VARCHAR (15) NOT NULL UNIQUE,
+`established_on` DATE
+);
+
+INSERT INTO `manufacturers`(`name`, `established_on`)
+VALUES
+('BMW','1916-03-01'),
+('Tesla','2003-01-01'),
+('Lada','1966-05-01');
+
+CREATE TABLE `models`(
+`model_id` INT(10) NOT NULL PRIMARY KEY,
+`name` VARCHAR(30) NOT NULL,
+`manufacturer_id` INT,
+CONSTRAINT fk_manufacturer
+FOREIGN KEY (`manufacturer_id`)
+REFERENCES `manufacturers`(`manufacturer_id`)
+);
+
+INSERT INTO `models` (`model_id`,`name`,`manufacturer_id`)
+VALUES
+(101,'X1',1),
+(102,'i6',1),
+(103,'Model S',2),
+(104,'Model X',2),
+(105,'Model 3',2),
+(106,'Nova',3);
