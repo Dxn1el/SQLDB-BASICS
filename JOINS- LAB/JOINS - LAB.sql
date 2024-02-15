af8df93b87c2b85c545e5c8bd593f7c3cee49bdc
Dxn1el <126982891+Dxn1el@users.noreply.github.com>
2024-02-15 18:41:43 +0200

    Update JOINS - LAB.sql

--- JOINS- LAB/JOINS - LAB.sql
+++ JOINS- LAB/JOINS - LAB.sql
@@ -19,3 +19,36 @@ WHERE
             AVG(e1.salary)
         FROM
             employees AS e1);
+
+
+
+# addresses
+
+
+SELECT 
+    t.town_id, t.name, a.address_text
+FROM
+    addresses AS a
+        JOIN
+    towns AS t ON t.town_id = a.town_id
+WHERE
+    t.name IN ('San Francisco' , 'Sofia', 'Carnation')
+ORDER BY t.town_id , a.address_id;
+
+SELECT * FROM addresses;
+
+#  employees without manager
+
+SELECT 
+    e.employee_id,
+    e.first_name,
+    e.last_name,
+    e.department_id,
+    e.salary
+FROM
+    employees AS e
+        RIGHT JOIN
+    departments AS d ON e.department_id = d.department_id
+WHERE
+    e.manager_id IS NULL;
+
