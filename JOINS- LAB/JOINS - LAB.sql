56fd59f235b91c303f8f5d58ed408dcb244b6306
Dxn1el <126982891+Dxn1el@users.noreply.github.com>
2024-02-15 17:26:31 +0200

    Update JOINS - LAB.sql

---
+++ JOINS- LAB/JOINS - LAB.sql
@@ -0,0 +1,14 @@
+# MANAGERS
+SELECT 
+    d.manager_id 'employee_id',
+    concat_ws(' ', e.first_name, e.last_name) 'full_name',
+    d.department_id department_id,
+    d.name 'department_name'
+FROM
+    departments AS d
+        JOIN
+    employees AS e ON d.manager_id = e.employee_id
+ORDER BY e.employee_id
+LIMIT 5;
+
+#HIGHEST SALARY COUNT
