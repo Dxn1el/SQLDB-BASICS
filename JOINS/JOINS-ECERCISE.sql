53a2d651b4552636d8a0dc25d5a21ad742643ba5
Dxn1el <126982891+Dxn1el@users.noreply.github.com>
2024-02-16 14:58:49 +0200

    Update JOINS-ECERCISE.sql

--- JOINS/JOINS-ECERCISE.sql
+++ JOINS/JOINS-ECERCISE.sql
@@ -18,4 +18,6 @@ limit 5;
 SELECT e.first_name,e.last_name,t.name, a.address_text
 FROM employees as e
 JOIN addresses as a ON e.address_id = a.address_id
-JOIN towns as t ON a.town_id = t.town_id;
+JOIN towns as t ON a.town_id = t.town_id
+ORDER BY first_name ASC , last_name ASC
+LIMIT 5;
