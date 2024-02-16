f80a65f176c2e05009f1b89b39bb5773b726180c
Dxn1el <126982891+Dxn1el@users.noreply.github.com>
2024-02-16 16:39:21 +0200

    Update JOINS-ECERCISE.sql

--- JOINS/JOINS-ECERCISE.sql
+++ JOINS/JOINS-ECERCISE.sql
@@ -45,19 +45,34 @@ limit 5
 SELECT e.employee_id,
        e.first_name
 from employees as e
-LEFT JOIN employees_projects as p ON e.employee_id = p.project_id
+         LEFT JOIN employees_projects as p ON e.employee_id = p.project_id
 WHERE project_id IS NULL
 ORDER BY e.employee_id DESC
 Limit 3;
 
 #Employees Hired After
-SELECT  e.first_name, e.last_name,e.hire_date, d.name as  dept_name
+SELECT e.first_name, e.last_name, e.hire_date, d.name as dept_name
 FROM employees AS e
-JOIN departments as d ON e.department_id = d.department_id
-WHERE hire_date>'1999/01/01' AND (d.name = 'finance' OR d.name='sales')
+         JOIN departments as d ON e.department_id = d.department_id
+WHERE hire_date > '1999/01/01'
+  AND (d.name = 'finance' OR d.name = 'sales')
 ORDER BY e.hire_date ASC;
 
 #Employees with Project
+
+SELECT e.employee_id,e.first_name, p.name
+FROM employees as e
+          JOIN employees_projects as ep ON e.employee_id = ep.employee_id
+         JOIN projects as p on ep.project_id = p.project_id
+#Filter only employees with a project, which has started after 13.08.2002 and it is still ongoing (no end date).
+WHERE DATE(start_date) > '2002-08-13 00:00:00'
+  AND end_date IS NULL
+# Return the first 5 rows sorted by first_name then by project_name both in ascending order.
+ORDER BY e.first_name, p.name
+LIMIT 5;
+
+#Employee 24
+
 
 
 
