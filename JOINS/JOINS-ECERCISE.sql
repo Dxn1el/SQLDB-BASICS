5c8e36adbf5c8de563e26802d7141c3ddd4bae39
Dxn1el <126982891+Dxn1el@users.noreply.github.com>
2024-02-16 15:05:37 +0200

    Update JOINS-ECERCISE.sql

--- JOINS/JOINS-ECERCISE.sql
+++ JOINS/JOINS-ECERCISE.sql
@@ -2,22 +2,19 @@
 
 SELECT e.employee_id, e.job_title, e.address_id, a.address_text
 FROM employees as e
-INNER JOIN addresses as a ON e.address_id = a.address_id
+         INNER JOIN addresses as a ON e.address_id = a.address_id
 ORDER BY address_id asc
 limit 5;
 
 # Addresses with Towns
 
-#Write a query that selects:
-# first_name
-#last_name
-#town
-#address_text
-#Sort the result by first_name in ascending order then by last_name. Select first 5 employees.
 
-SELECT e.first_name,e.last_name,t.name, a.address_text
+SELECT e.first_name, e.last_name, t.name, a.address_text
 FROM employees as e
-JOIN addresses as a ON e.address_id = a.address_id
-JOIN towns as t ON a.town_id = t.town_id
-ORDER BY first_name ASC , last_name ASC
+         JOIN addresses as a ON e.address_id = a.address_id
+         JOIN towns as t ON a.town_id = t.town_id
+ORDER BY first_name ASC, last_name ASC
 LIMIT 5;
+
+# SALES EMPLOYEE
+
