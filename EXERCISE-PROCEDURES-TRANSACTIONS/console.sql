f5b34ff5fb833f3568c6997b7435e863838a96d2
Dxn1el <126982891+Dxn1el@users.noreply.github.com>
2024-03-31 14:21:01 +0300

    Update console.sql

--- EXERCISE-PROCEDURES-TRANSACTIONS/console.sql
+++ EXERCISE-PROCEDURES-TRANSACTIONS/console.sql
@@ -1,74 +1,129 @@
-#EMPLOYEES >35000
-CREATE PROCEDURE usp_get_employees_salary_above_35000()
-begin
-    SELECT first_name, last_name
-    FROM employees
-    WHERE salary > 35000
-    order by first_name, last_name, employee_id;
-end;
+#8
 
-# test
-call soft_uni.usp_get_employees_salary_above_35000();
-
-#EMPLOYEES WITH SALARY ABOVE NUMBER
-
-CREATE PROCEDURE usp_get_employees_salary_above(target_salary DECIMAL (19,4))
-begin
-    SELECT  first_name, last_name FROM employees
-        where salary >=target_salary
-    ORDER BY first_name,last_name,employee_id;
-end;
-
-#TEST
-call soft_uni.usp_get_employees_salary_above(100000.0000);
-
-#TOWN NAMES  STARTING WITH STRING
-CREATE PROCEDURE  usp_get_towns_starting_with(starting_string VARCHAR(25))
-begin
-SELECT  name FROM towns
-    WHERE name like concat(starting_string, '%')
-    ORDER BY name;
-end;
-
-#TEST
-call soft_uni.usp_get_towns_starting_with('b');
-
-#Employees from Town
-CREATE PROCEDURE usp_get_employees_from_town (string_town VARCHAR (25))
-begin
-SELECT first_name, last_name FROM employees as e
-    JOIN addresses as a USING (address_id)
-    JOIN towns as t USING (town_id)
-
-WHERE t.name = string_town
-ORDER BY first_name, last_name;
-end;
-
-#TEST
-call soft_uni.usp_get_employees_from_town('Sofia');
-
-
-#Salary Level Function
-CREATE FUNCTION  ufn_get_salary_level (employee_salary DECIMAL(19,4))
-RETURNS VARCHAR(20)
-DETERMINISTIC
+CrEATE PROCEDURE usp_get_holders_full_name()
 BEGIN
-    DECLARE salary_level VARCHAR(10);# text with the level of salary
-    IF employee_salary <  30000 THEN set salary_level :='low';
-    ELSEIF  employee_salary <=50000 then  set salary_level :='average';
-    ELSE  set salary_leveL :='high';
-    end if;
-    return salary_level;
+    SELECT concat(first_name, ' ', last_name) as full_name
+    from account_holders
+    order by full_name, id;
+end;
+
+#9
+create procedure usp_get_holders_with_balance_higher_than(money DECIMAL(19, 4))
+begin
+    select ah.first_name, ah.last_name
+    FROM account_holders AS ah
+             left join accounts as a on ah.id = a.account_holder_id
+    group by ah.first_name, ah.last_name
+    HAVING SUM(a.balance) > money
+    order by a.account_holder_id;
+end;
+
+#10
+CREATE FUNCTION ufn_calculate_future_value(sum DECIMAL(19, 4), yearly_rate DOUBLE, years INT)
+    returns DECIMAL(19, 4)
+    deterministic
+begin
+    DECLARE future_sum DECIMAL(19, 4);
+    SET future_sum := sum * POW(1 + yearly_rate, years);
+    return future_sum;
 
 end;
 
-#Employees by Salary Level
-CREATE PROCEDURE usp_get_employees_by_salary_level(salary_lever VARCHAR(10))
-begin
-    SELECT first_name, last_name from employees
-    WHERE ufn_get_salary_level(salary) = salary_lever
-    ORDER BY first_name, last_name;
-    end;
+#11
+CREATE PROCEDURE usp_calculate_future_value_for_account(id INT, rate DECIMAL(19, 4))
+BEGIN
+    select a.id,
+           ah.first_name,
+           ah.last_name,
+           balance,
+           ufn_calculate_future_value(a.balance, rate, 5)
+    from account_holders as ah
+             join accounts as a on ah.id = a.account_holder_id;
+    WHERE a.id = id;
+end;
 
-#Define Function
+CREATE PROCEDURE usp_deposit_money(account_id INT, money_amount DECIMAL(19, 4))
+begin
+    START TRANSACTION ;
+    IF (money_amount <= 0) THEN
+        ROLLBACK ;
+    ELSE
+        UPDATE accounts set balance = balance + money_amount;
+        where id=account_id;
+    end if;
+end;
+
+CREATE PROCEDURE usp_withdraw_money(account_id INT, money_amount DECIMAL(19, 4))
+BEGIN
+    START TRANSACTION ;
+    IF (money_amount <= 0 OR (select balance from accounts where id = account_id)) then
+        rollback;
+    else
+        update accounts set balance= balance - money_amount;
+    end if;
+end;
+
+#14
+create procedure usp_transfer_money(from_id INT, to_id INT, sum DECIMAL(19, 4))
+BEGIN
+    START TRANSACTION;
+    # VALID ID
+    IF from_id = to_id OR
+       sum <= 0 OR
+       (SELECT balance from accounts where id = from_id) < sum OR
+       (select count(id) from accounts where id = from_id) <> 1 OR
+       (select count(id) from accounts where id = to_id) <> 1
+    then
+        rollback ;
+    ELSE
+        UPDATE accounts
+        set balance = balance - sum
+        where id = from_id;
+        UPDATE accounts
+        set balance = balance + sum
+        where id = from_id;
+        commit;
+    END IF;
+end;
+
+#15 LOG ACCOUNTS TRIGGER
+CREATE TABLe logs
+(
+    log_id     int primary key auto_increment,
+    account_id int            not null,
+    old_sum    decimal(19, 4) not null,
+    new_sum    decimal(19, 4) not null
+);
+CREATE TRIGGER tr_change_balance
+    AFTER UPDATE
+    ON accounts
+    FOR EACH ROW
+BEGIN
+    INSERT INTO logs (account_id, old_sum, new_sum)
+    VALUES (OLD.id, OLD.balance, NEW.balance);
+END;
+
+#16 EMAILS TRIGGER
+create table notification_emails
+(
+    id        int not null primary key auto_increment,
+    recipient INT NOT NULL,
+    subject   TEXT,
+    body      TEXT  
+);
+
+CREATE TRIGGER tr_email_on_update
+    AFTER INSERT
+    on logs
+    FOR EACH ROW
+begin
+    INSERT INTO notification_emails (recipient, subject, body)
+    values (
+            new.account_id,
+            concat('Balance change for account: ', account_id),
+            concat('ON', now(), 'Your balance was changed from', new.old_sum, ' to ', new.new_sum, '.')
+           );
+
+end ;
+
 
